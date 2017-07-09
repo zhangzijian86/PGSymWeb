bbd7932961855ef26de070481720c75baf093611
zhangzijian86 <zhangzijian86@163.com>
2017-07-09 11:36:06 +0800

    all ok

--- src/com/sym/servlet/DeleteUser.java
+++ src/com/sym/servlet/DeleteUser.java
@@ -25,6 +25,7 @@ public class DeleteUser extends HttpServlet {
 	
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {		
+		response.setHeader("Access-Control-Allow-Origin", "*");
 		response.setContentType("text/html");
 		request.setCharacterEncoding("utf-8");
 		response.setCharacterEncoding("utf-8");
